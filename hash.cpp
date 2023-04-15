77e32db8c0a791ac03ea6eed0324778d192cf933
Michael <michael.walton@g.austincc.edu>
2023-04-15 13:39:47 -0500

    stubbed class methods

---
+++ hash.cpp
@@ -0,0 +1,36 @@
+/*********************
+Name: Michael Walton
+COSC-2436-004
+Purpose:
+ - Design Hash Table ADT with Array of Head Pointers
+**********************/
+
+#include "hash.h"
+
+HashTable::HashTable() {
+    count = 0;
+}
+
+HashTable::~HashTable() {}
+
+bool HashTable::addEntry(int, int *) {
+    return true;
+}
+
+bool HashTable::removeEntry(int) {
+    return true;
+}
+
+int HashTable::getEntry(int) {
+    return 0;
+}
+
+int HashTable::getCount() {
+    return 0;
+}
+
+void HashTable::displayTable() {}
+
+int HashTable::hash(int) {
+    return 0;
+}
