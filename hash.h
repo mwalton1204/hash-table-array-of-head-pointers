4bfd98c29fdc7c0ffacbbcacbddbac90db31ef06
Michael <michael.walton@g.austincc.edu>
2023-04-15 13:37:13 -0500

    defined standard data and node structs. wrote prototype for HashTable class

---
+++ hash.h
@@ -0,0 +1,47 @@
+/*********************
+Name: Michael Walton
+COSC-2436-004
+Purpose:
+ - Design Hash Table ADT with Array of Head Pointers
+**********************/
+
+#ifndef HASH_H
+#define HASH_H
+
+#define HASHTABLESIZE 15
+
+//Standard Data Struct
+struct Data {
+    int id;
+    string data;
+};
+
+//Standard Node Struct
+struct Node {
+    Data data;
+    Node *next;
+};
+
+class HashTable {
+
+public:
+
+    HashTable();
+    ~HashTable();
+
+    bool addEntry(int, string*);
+    bool removeEntry(int);
+    string getEntry(int);
+    int getCount();
+    void displayTable();
+
+private:
+
+    int hash(int);
+
+    int count;
+    Node *hashTable[HASHTABLESIZE];
+
+};
+
+#endif //HASH_H
